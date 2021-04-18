6bc447d3249cc71a28c413a583a623c5e6e0f085
Ali Ghorashi <aligho@mit.edu>
2021-04-18 14:49:09 -0400

    minor comments

--- src/export_wannier_hamiltonians.jl
+++ src/export_wannier_hamiltonians.jl
@@ -10,11 +10,9 @@ function write_map_write_h(cell_map::String, cell_weights::String, H::String, km
         kfold=np.array([kmesh[0], kmesh[1], kmesh[2]])
         kfoldProd = np.prod(kfold)
         kStride = np.array([kfold[1]*kfold[2], kfold[2], 1])
-
         HreducedUp = np.fromfile(H).reshape((kfoldProd,nBandsUp,nBandsUp)).swapaxes(1,2)
         iReducedUp = np.dot(np.mod(cellMapUp, kfold[None,:]), kStride)
         HwannierUp = WwannierUp * HreducedUp[iReducedUp]
-
         np.savetxt(band_file, HwannierUp.reshape(len(iReducedUp), nBandsUp*nBandsUp ))
         np.savetxt(cell_map_file, cellMapUp)
     """
@@ -52,11 +50,9 @@ function write_map_write_h(filebase::String, kmesh::Array{<:Real, 1}; spin::Unio
         kfold=np.array([kmesh[0], kmesh[1], kmesh[2]])
         kfoldProd = np.prod(kfold)
         kStride = np.array([kfold[1]*kfold[2], kfold[2], 1])
-
         HreducedUp = np.fromfile(H).reshape((kfoldProd,nBandsUp,nBandsUp)).swapaxes(1,2)
         iReducedUp = np.dot(np.mod(cellMapUp, kfold[None,:]), kStride)
         HwannierUp = WwannierUp * HreducedUp[iReducedUp]
-
         np.savetxt(band_file, HwannierUp.reshape(len(iReducedUp), nBandsUp*nBandsUp ))
         np.savetxt(cell_map_file, cellMapUp)
     """
@@ -72,12 +68,10 @@ function write_momentum(cell_map::String, cell_weights::String, H::String, P::St
         nCells = cellMap.shape[0]
         nBands = int(np.sqrt(Wwannier.shape[0] / nCells))
         Wwannier = Wwannier.reshape((nCells,nBands,nBands)).swapaxes(1,2)
-
         kfold=np.array([kmesh[0], kmesh[1], kmesh[2]])
         kfoldProd = np.prod(kfold)
         kStride = np.array([kfold[1]*kfold[2], kfold[2], 1])
         Hreduced = np.fromfile(H).reshape((kfoldProd,nBands,nBands)).swapaxes(1,2)
-
         Preduced = np.fromfile(P).reshape((kfoldProd,3,nBands,nBands)).swapaxes(2,3)
         iReduced = np.dot(np.mod(cellMap, kfold[None,:]), kStride)
         Hwannier = Wwannier * Hreduced[iReduced]
@@ -111,18 +105,15 @@ function write_momentum(filebase::String, kmesh::Array{Int, 1}, momentum_file::S
         nCells = cellMap.shape[0]
         nBands = int(np.sqrt(Wwannier.shape[0] / nCells))
         Wwannier = Wwannier.reshape((nCells,nBands,nBands)).swapaxes(1,2)
-
         kfold=np.array([kmesh[0], kmesh[1], kmesh[2]])
         kfoldProd = np.prod(kfold)
         kStride = np.array([kfold[1]*kfold[2], kfold[2], 1])
         Hreduced = np.fromfile(H).reshape((kfoldProd,nBands,nBands)).swapaxes(1,2)
-
         Preduced = np.fromfile(P).reshape((kfoldProd,3,nBands,nBands)).swapaxes(2,3)
         iReduced = np.dot(np.mod(cellMap, kfold[None,:]), kStride)
         Hwannier = Wwannier * Hreduced[iReduced]
         Pwannier = Wwannier[:,None] * Preduced[iReduced]
         np.savetxt(momentum_file, Pwannier.reshape(len(iReduced), 3*nBands*nBands ))
- 
     """
     py"write_map_write_p"(cell_map, cell_weights, H, P, kmesh, momentum_file)
 end
@@ -135,22 +126,17 @@ function write_eph_matrix_elements(cell_map::String, cell_weights::String, cell_
         Wwannier = np.fromfile(cell_weights)
         nCells = cellMap.shape[0]
         nBands = int(np.sqrt(Wwannier.shape[0] / nCells))
-
         cellMapEph = np.loadtxt(cell_map_ph, usecols=[0,1,2]).astype(int)
         nCellsEph = cellMapEph.shape[0]
-
         prodPhononSup = np.prod(qmesh)
         phononSupStride = np.array([qmesh[1]*qmesh[2], qmesh[2], 1])
-
-        nAtoms = nModes // 3
+        nAtoms = nModes // 3 #Rounding down, since we know number of modes must be a multiple of three
         cellWeightsEph = np.fromfile(cell_map_ph_weights).reshape((nCellsEph,nBands,nAtoms)).swapaxes(1,2)
         cellWeightsEph = np.repeat(cellWeightsEph.reshape((nCellsEph,nAtoms,1,nBands)), 3, axis=2) #repeat atom weights for 3 directions
         cellWeightsEph = cellWeightsEph.reshape((nCellsEph,nModes,nBands)) #coombine nAtoms x 3 into single dimension: nModes
-
         iReducedEph = np.dot(np.mod(cellMapEph, qmesh[None,:]), phononSupStride)
         HePhReduced = np.fromfile(HPh).reshape((prodPhononSup,prodPhononSup,nModes,nBands,nBands)).swapaxes(3,4)
         HePhWannier = cellWeightsEph[:,None,:,:,None] * cellWeightsEph[None,:,:,None,:] * HePhReduced[iReducedEph][:,iReducedEph]
-        
         return HePhWannier, cellMapEph
     """
     py"write_eph"(cell_map, cell_weights, cell_map_ph, cell_map_ph_weights, HPh, nModes, qmesh)
@@ -165,7 +151,6 @@ function write_eph_matrix_elements(filebase::String, nModes::Int, qmesh::Array{I
     cell_map_ph = "$filebase.mlwfCellMapPh"
     cell_map_ph_weights = "$filebase.mlwfCellWeightsPh"
     HPh = "$filebase.mlwfHePh"
-
     if spin isa Val{'u'}
         cell_map = "$filebase.mlwfCellMapUp"
         cell_weights = "$filebase.mlwfCellWeightsUp"
@@ -179,7 +164,6 @@ function write_eph_matrix_elements(filebase::String, nModes::Int, qmesh::Array{I
         cell_map_ph_weights = "$filebase.mlwfCellWeightsPhDn"
         HPh = "$filebase.mlwfHePhDn"    
     end
-
     py"""
     def write_eph(cell_map, cell_weights, cell_map_ph, cell_map_ph_weights, HPh, nModes, qmesh):
         import numpy as np
@@ -187,27 +171,20 @@ function write_eph_matrix_elements(filebase::String, nModes::Int, qmesh::Array{I
         Wwannier = np.fromfile(cell_weights)
         nCells = cellMap.shape[0]
         nBands = int(np.sqrt(Wwannier.shape[0] / nCells))
-
         cellMapEph = np.loadtxt(cell_map_ph, usecols=[0,1,2]).astype(int)
         nCellsEph = cellMapEph.shape[0]
-
         prodPhononSup = np.prod(qmesh)
         phononSupStride = np.array([qmesh[1]*qmesh[2], qmesh[2], 1])
-
-        nAtoms = nModes // 3
+        nAtoms = nModes // 3 # Rounding down to an integer, since we know the number of modes is a multiple of three
         cellWeightsEph = np.fromfile(cell_map_ph_weights).reshape((nCellsEph,nBands,nAtoms)).swapaxes(1,2)
         cellWeightsEph = np.repeat(cellWeightsEph.reshape((nCellsEph,nAtoms,1,nBands)), 3, axis=2) #repeat atom weights for 3 directions
         cellWeightsEph = cellWeightsEph.reshape((nCellsEph,nModes,nBands)) #coombine nAtoms x 3 into single dimension: nModes
-
         iReducedEph = np.dot(np.mod(cellMapEph, qmesh[None,:]), phononSupStride)
         HePhReduced = np.fromfile(HPh).reshape((prodPhononSup,prodPhononSup,nModes,nBands,nBands)).swapaxes(3,4)
         HePhWannier = cellWeightsEph[:,None,:,:,None] * cellWeightsEph[None,:,:,None,:] * HePhReduced[iReducedEph][:,iReducedEph]
-        
         return HePhWannier, cellMapEph
-
     """
     py"write_eph"(cell_map, cell_weights, cell_map_ph, cell_map_ph_weights, HPh, nModes, qmesh)
-
 end
 
 
