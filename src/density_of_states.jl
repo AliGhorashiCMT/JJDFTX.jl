34364e1fd318d183ce6f9661f3570ae61c8c4ead
Ali Ghorashi <aligho@mit.edu>
2021-05-02 16:39:58 -0400

    returntot

--- src/density_of_states.jl
+++ src/density_of_states.jl
@@ -85,11 +85,13 @@ function density_of_states(dosfile_1::String, dosfile_2::String; kwargs... )
     end
 end
 
-function density_of_states(dosfile_1::String; kwargs...)
+function density_of_states(dosfile_1::String; returntot::Bool=false, kwargs...)
     try
-        plot(np.loadtxt(dosfile_1)[:, 1]*1/eV, np.loadtxt(dosfile_1)[:, 2]*eV, linewidth=4, size=(800, 400), xlims = (-2,-0.5), ylims = (0,500/27.2), label="Spin Unpolarized"; kwargs...)
+        display(plot(np.loadtxt(dosfile_1)[:, 1]*1/eV, np.loadtxt(dosfile_1)[:, 2]*eV, linewidth=4, size=(800, 400), xlims = (-2,-0.5), ylims = (0,500/27.2), label="Spin Unpolarized"; kwargs...))
+        returntot && println("Total number of electrons is: ", sum(diff(np.loadtxt(dosfile_1)[:, 1]), np.loadtxt(dosfile_1)[2:end, 2]  ) )
     catch 
-        plot(np.loadtxt(dosfile_1, skiprows=1)[:, 1]*1/eV, np.loadtxt(dosfile_1, skiprows=1)[:, 2]*eV, linewidth=4, size=(800, 400), xlims = (-2,-0.5), ylims = (0,500/27.2), label="Spin Unpolarized"; kwargs...)
+        display(plot(np.loadtxt(dosfile_1, skiprows=1)[:, 1]*1/eV, np.loadtxt(dosfile_1, skiprows=1)[:, 2]*eV, linewidth=4, size=(800, 400), xlims = (-2,-0.5), ylims = (0,500/27.2), label="Spin Unpolarized"; kwargs...))
+        returntot ? println("Total number of electrons is: ", sum(diff(np.loadtxt(dosfile_1, skiprows=1)[:, 1]) .* np.loadtxt(dosfile_1, skiprows=1)[2:end, 2])) : nothing
     end
 end
 
@@ -199,6 +201,9 @@ function density_of_states_wannier_quad_check(HWannier::Array{Float64, 3}, cell_
     return sum(ϵdif*dosarray)
 end
 
+"""
+$(TYPEDSIGNATURES)
+"""
 function density_of_states_wannier(wannier_file::String, cell_map_file::String; mesh::Int = 100, histogram_width::Real = 100, energy_range::Real = 10, offset::Real = 0)
     WannierDOS=np.zeros(histogram_width*energy_range)
     for x_mesh in 1:mesh
@@ -308,6 +313,7 @@ function wannierbandsoverlayedDOS(HWannier::Array{Float64, 3}, cell_map::Array{F
             WannierDOS[round(Int, histogram_width*(ϵ+offset))]=WannierDOS[round(Int, histogram_width*(ϵ+offset))]+spin*histogram_width*(1/mesh)^2
         end
     end
+    println(sum(WannierDOS.*1/histogram_width) )
     @assert sum(WannierDOS.*1/histogram_width) ≈ nbands*spin #Check normalization of density of states per unit cell.
     A = plot(energiesatkpoints, ylims=[-offset, energy_range-offset], xticks = false, legend=false, ylabel = "Energy (eV)")
     B = plot( WannierDOS, collect(1:histogram_width*energy_range)./histogram_width .-offset, legend=false, xlabel = "DOS (1/eV)", yticks = false)
@@ -448,18 +454,16 @@ end
 
 function density_of_states_wannier(HWannier::Array{Float64, 3}, cell_map::Array{Float64, 2}, nbands::Int; exclude_bands = Int[], mesh::Int = 100, histogram_width::Int = 100, energy_range::Real = 10, offset::Real = 0)
     WannierDOS=np.zeros(histogram_width*energy_range)
-    for x_mesh in 1:mesh
-        for y_mesh in 1:mesh
-            ϵ=  wannier_bands(HWannier, cell_map, [x_mesh/mesh, y_mesh/mesh, 0], nbands)
-            for band in 1:nbands
-                if band ∉ exclude_bands
-                    ϵ_band = ϵ[band]
-                    WannierDOS[round(Int, histogram_width*(ϵ_band+offset))]=WannierDOS[round(Int, histogram_width*(ϵ_band+offset))]+histogram_width*(1/mesh)^2
-                end
-            end
+    energies = collect(0:1/histogram_width:energy_range-1/histogram_width) .- offset
+    for (xmesh, ymesh) in Tuple.(CartesianIndices(rand(mesh, mesh)))
+        ϵs =  wannier_bands(HWannier, cell_map, [xmesh/mesh, ymesh/mesh, 0], nbands)
+        for (band, ϵ) in enumerate(ϵs)
+            (band ∈ exclude_bands) && continue
+            WannierDOS[round(Int, histogram_width*(ϵ+offset))]=WannierDOS[round(Int, histogram_width*(ϵ+offset))]+histogram_width*(1/mesh)^2
         end
     end
-    return WannierDOS
+    @assert sum(WannierDOS.*1/histogram_width) ≈ nbands #Verify normalization 
+    return energies, WannierDOS
 end
 
 function density_of_states_montecarlo(HWannier::Array{Float64, 3}, cell_map::Array{Float64, 2}, nbands::Int; exclude_bands = Int[], mesh::Int = 100, histogram_width::Int = 100, energy_range::Real = 10, offset::Real = 0)
