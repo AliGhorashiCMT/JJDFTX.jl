c595006221d378d1a56486b3fa58d7437ca79ccc
Ali Ghorashi <aligho@mit.edu>
2021-05-02 18:46:37 -0400

    graphene phonon heat capacity

--- src/heat_capacities.jl
+++ src/heat_capacities.jl
@@ -60,6 +60,16 @@ end
 
 """
 $(TYPEDSIGNATURES)
+"""
+function graphene_phononheatcapacity(T::Real)
+    #Take sound velocity to be 20 km/s -> 2*10^14 angstroms/second. Therefore, dispersion is given by 6.6*2*10^(-2)*k 
+    # Take a cutoff wavevector of 1 angstroms^(-1). 
+    prefactor = (1/(1.3*6.6e-2)^2)*(2/(4*π^2))+(1/(2*6.6e-2)^2)*(1/(4*π^2)) # Different logitudinal and transverse sound velocities. 
+    prefactor*sum(bosederivative.(collect(0.0001:0.001:0.16), Ref(T)).*collect(0.0001:0.001:0.16))*0.001
+end
+
+"""
+$(TYPEDSIGNATURES)
 
 """
 function electron_heatcapacities(μ::Real, Ts::Union{Vector{<:Real}, StepRange{<:Integer, <:Integer}, UnitRange{<:Integer}}, lat::Vector{<:Vector{<:Real}}, HWannier::Array{Float64, 3}, cell_map::Array{Float64, 2}, nbands::Integer; exclude_bands = Int[], mesh::Int = 100, histogram_width::Int = 100, energy_range::Real = 10, offset::Real = 0)
@@ -124,3 +134,35 @@ function lattice_heatcapacity(T::Real, lat::Vector{<:Vector{<:Real}}, forcematri
     @assert (sum(PhononDOS ./ histogram_width) ≈ nmodes) #Check Normalization
     return (sum(DOSweightedϵ ./ histogram_width))
 end
+
+"""
+$(TYPEDSIGNATURES)
+Returns the heat capacity in eV/(Kelvin*angstrom^2). For two dimensional lattices.
+"""
+function lattice_heatcapacity(T::Real, lat::Vector{<:Vector{<:Real}}, forcematrix::Array{<:Real, 3}, phononcellmap::Array{<:Real, 2}, ::Val{2}; mesh::Integer=100, histogram_width::Real=1000, energy_range::Real=0.3)
+    PhononDOS=np.zeros(round(Int, histogram_width*energy_range))
+    DOSweightedϵ=np.zeros(round(Int, histogram_width*energy_range))
+    vol = unit_cell_area(lat)
+    nmodes = length(phonon_dispersion(forcematrix, phononcellmap, [0, 0, 0]))
+    for (xmesh, ymesh) in Tuple.(CartesianIndices(rand(mesh, mesh)))
+        ωs=  phonon_dispersion(forcematrix, phononcellmap, [xmesh/mesh, ymesh/mesh, 0])
+        for ω in ωs
+            if ω>0
+                PhononDOS[round(Int, histogram_width*ω)+1]=PhononDOS[round(Int, histogram_width*ω)+1]+histogram_width*(1/mesh)^2
+                DOSweightedϵ[round(Int, histogram_width*ω)+1]=DOSweightedϵ[round(Int, histogram_width*ω)+1]+ω*bosederivative(ω, T)*histogram_width*1/vol*(1/mesh)^2
+            end
+        end
+    end
+    @assert (sum(PhononDOS ./ histogram_width) ≈ nmodes) #Check Normalization
+    return (sum(DOSweightedϵ ./ histogram_width))
+end
+
+function lattice_heatcapacities(Ts::Union{Vector{<:Real}, StepRange{<:Integer, <:Integer}, UnitRange{<:Integer}}, lat::Vector{<:Vector{<:Real}}, forcematrix::Array{<:Real, 3}, phononcellmap::Array{<:Real, 2}, ::Val{2}; mesh::Integer=100, histogram_width::Real=1000, energy_range::Real=0.3)
+    cs = Float64[]
+    for T in Ts
+        println("T: ", T)
+        push!(cs, lattice_heatcapacity(T, lat, forcematrix, phononcellmap, Val(2); mesh=mesh, histogram_width=histogram_width, energy_range=energy_range))
+    end
+    return cs
+end
+
