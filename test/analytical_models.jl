e2e59e215ba46415ea5d07a8099dbafb878ed540
Ali Ghorashi <aligho@mit.edu>
2021-05-22 19:33:36 -0400

    missing begin in testest

--- test/analytical_models.jl
+++ test/analytical_models.jl
@@ -1,4 +1,4 @@
-@testset "graphene conductivity"
+@testset "graphene conductivity" begin
     @test isapprox(1, imag(JJDFTX.graphene_conductivity(1, 1/50, 3)), atol=1e-1)
     @test isapprox(0, imag(JJDFTX.graphene_conductivity(1, 1/10, 1, atol=1e-10, delta=0.001), atol=1e-2)
 end
