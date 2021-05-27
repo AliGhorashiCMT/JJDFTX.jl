b713672a43b96deefddd20d8a734c91f88a14441
Ali Ghorashi <aligho@mit.edu>
2021-05-27 16:03:13 -0400

    Added Additional test for graphene

--- test/analytical_models.jl
+++ test/analytical_models.jl
@@ -18,4 +18,12 @@
     b = JJDFTX.real_neutral(q, 0.06)
     @test abs((a-b)/a*100) < 1 #Check for less than 1 percent difference
 
+
+    #------------------------------------------------------------------------------------------------------------------------------------#
+
+    #Kramers Kronig Transformations for arbitrary chemical potentials
+    a = kramers_kronig(ω->JJDFTX.graphene_total_impolarization(q, ω, 1), 1.3,  60000, min_energy_integration=0)
+    b=graphene_total_polarization(q, 1.3, 1)
+    @test abs((a-b)/a)*100 < 1 #Less than 1 percent difference
+
 end
