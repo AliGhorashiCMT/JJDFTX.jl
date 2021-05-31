a6d22a9e9889ed2779bf683472417f50ed3b2b2e
Ali Ghorashi <aligho@mit.edu>
2021-05-31 16:41:03 -0400

    Two Plasmon Absorption Img and Test

--- src/loss_calculations/analyticlossmodels.jl
+++ src/loss_calculations/analyticlossmodels.jl
@@ -3,59 +3,48 @@ $(TYPEDSIGNATURES)
 
 Calculate the two plasmon absorption rate of graphene within the Dirac cone approximation. 
 
-This calculates F2(ω) as defined in Multi-plasmon absorption in graphene by Jablan and Chang
+This returns the nondimensional F2(ω) as defined in the paper Multi-plasmon absorption in graphene by Jablan and Chang
 """
-function graphenetwoplasmonemission(ω::Real, μ::Real; mesh = 100, histogram_width =10, δ=1, conesize=1)
+function graphenetwoplasmonemission(ω::Real, μ::Real; mesh = 100, histogram_width =10, δ=1, conesize=1, verbose::Bool = true)
     F2 = 0 
-    ucellarea = 5.238760872572826 #Graphene unit cell area in angstrom^2 
     kF = μ/6
-    q= 2π*2.5*137/(4*pi*ħ*c*μ)*ω^2 #ev^2/ev^2/angstrom -> inverse angstrom units
-    println(q)
-    #prefactor = (μ^3/kF^2)*(π^2/ucellarea)*(ω/(6*q))^6
+    q = 2π*2.5*137/(4*pi*ħ*c*μ)*ω^2 #ev^2/ev^2/angstrom -> inverse angstrom units
+    #Note that we are taking an effective dielectric constant of 2.5- corresponding to a silica layer below the graphene 
+    #layer and vacuum on top of the graphene layer. 
+    verbose && println(q)
     prefactor = (μ^3/kF^2)*(π^2)*(ω/(6*q))^6
-    for kiter in 1:mesh
+    for (kiter, θiter) in Tuple.(CartesianIndices(rand(mesh, mesh)))
         k = μ/conesize*kiter/mesh
-        for θiter in 1:mesh
-            θ = 2π*θiter/mesh
-            for bandi in [-1, 1]
-                ϵi = 6*bandi*k
-                fi = heaviside(μ-ϵi)
-                fi ≈ 0 && continue
-                for bandf in [1]
-                    ϵf = 6*bandf*sqrt((k*cos(θ)+2*q)^2+k^2*sin(θ)^2)
-                    ff = heaviside(μ-ϵf)
-                    (1-ff) ≈ 0 && continue 
-
-                    ϵm = 6*sqrt((k*cos(θ)+q)^2+k^2*sin(θ)^2)
-                    #fm = heaviside(μ-ϵm)
-                    fm = 0 
-                    ϕi = θ
-                    ϕm = atan((k*sin(θ))/(k*cos(θ)+q))
-                    ϕf = atan((k*sin(θ))/(k*cos(θ)+2q))
-
-                    overlapim = 1+bandi*cis(ϕi-ϕm)
-                    overlapim *= 1/2
-                    overlapmf = 1+bandf*cis(ϕm-ϕf)
-                    overlapmf *= 1/2
-
-                    m1 = (1-fm)*(overlapim*overlapmf)/(ϵm-ϵi-ω-1im*δ)
-        
-                    ϵm = -6*sqrt((k*cos(θ)+q)^2+k^2*sin(θ)^2)
-                    #fm = heaviside(μ-ϵm)
-                    fm = 0 
-
-                    overlapim = 1-bandi*cis(ϕi-ϕm)
-                    overlapim *= 1/2
-                    overlapmf = 1-bandf*cis(ϕm-ϕf)
-                    overlapmf *= 1/2
-                    
-                    m2 = (1-fm)*overlapim*overlapmf/(ϵm-ϵi-ω-1im*δ)
-
-                    if abs(ϵf-ϵi-2*ω)*histogram_width<1
-                        #F2 +=  fi*(1-ff)*histogram_width/mesh^2*(abs(m1+m2))^2
-                        F2 +=  (1/(4*π))*(μ/conesize)^2*fi*(1-ff)*k*histogram_width/mesh^2*(abs(m1+m2))^2
-                    end
-                end
+        θ = 2π*θiter/mesh
+        for bandi in [-1, 1] #Initial band may be in either lower or upper bands
+            ϵi = 6*bandi*k
+            fi = heaviside(μ-ϵi)
+            fi ≈ 0 && continue
+            for bandf in [1] #All electronic final states are in upper band
+                ϵf = 6*bandf*sqrt((k*cos(θ)+2*q)^2+k^2*sin(θ)^2)
+                ff = heaviside(μ-ϵf)
+                (1-ff) ≈ 0 && continue 
+                ϵm = 6*sqrt((k*cos(θ)+q)^2+k^2*sin(θ)^2)
+                #fm = heaviside(μ-ϵm) #This should be the contribution, but we remain consistent with the paper cited
+                fm = 0
+                ϕi = θ
+                ϕm = atan((k*sin(θ))/(k*cos(θ)+q)) #We take q to lie on x axis WLOG 
+                ϕf = atan((k*sin(θ))/(k*cos(θ)+2q))
+                overlapim = 1/2*(1+bandi*cis(ϕi-ϕm))
+                overlapmf = 1/2*(1+bandf*cis(ϕm-ϕf))
+                m1 = (1-fm)*(overlapim*overlapmf)/(ϵm-ϵi-ω-1im*δ)
+                ϵm = -6*sqrt((k*cos(θ)+q)^2+k^2*sin(θ)^2)
+                #fm = heaviside(μ-ϵm) #This should be the contribution, but we remain consistent with the paper cited
+                fm = 0 
+                overlapim = 1/2*(1-bandi*cis(ϕi-ϕm))
+                overlapmf = 1/2*(1-bandf*cis(ϕm-ϕf))
+                m2 = (1-fm)*overlapim*overlapmf/(ϵm-ϵi-ω-1im*δ)
+                abs(ϵf-ϵi-2*ω)*histogram_width<0.5 || continue #Energy conserving delta function
+                F2 +=  (1/(4*π^2))*(μ/conesize)*(2π)*fi*(1-ff)*k*histogram_width/mesh^2*(abs(m1+m2))^2
+                #Explanation: the factors involving spin and valley degeneracy are included in the prefactor, so we have a 1/(2pi)^2 
+                #factor from the conversion of the sum to a 2d brillouin zone integral. 
+                #Note the interference between m1 and m2- which may add constructively or destructively in their contribution to the 
+                #overall loss. 
             end
         end
     end
