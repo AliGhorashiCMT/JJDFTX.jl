fecc28f784a37f1b27bf7bfac925708213e896ac
Ali Ghorashi <aligho@mit.edu>
2021-06-07 13:22:31 -0400

    cleaned up directory information

--- src/read_prepared_data/read_prepared_data.jl
+++ src/read_prepared_data/read_prepared_data.jl
@@ -8,16 +8,21 @@ function dft_graphene_dos_per_area(;kwargs...)
 end
 
 function dft_graphene_phonon_dispersion(q::Vector{<:Real})
-    cell_map_path = joinpath(@__DIR__, "../../data/graphene_examples/graphene.in.phononCellMap")
-    phonon_omegasq_path = joinpath(@__DIR__, "../../data/graphene_examples/graphene.in.phononOmegaSq")
+    dir =  "../../data/graphene_examples/"
+    cell_map_path = joinpath(@__DIR__, dir*"graphene.in.phononCellMap")
+    phonon_omegasq_path = joinpath(@__DIR__, dir*"graphene.in.phononOmegaSq")
     graphene_force_matrix, graphene_cell_map = phonon_force_matrix(cell_map_path, phonon_omegasq_path)
     return phonon_dispersion(graphene_force_matrix, graphene_cell_map, q)
 end
 
+"""
+Returns the graphene energy dispersion along the Γ-M-K-Γ path in the Brillouin zone.
+"""
 function dft_graphene_wannier_dispersion()
+    dir = "../../data/graphene_examples/"
     bands = zeros(8, 30)
-    bands_dir = joinpath(@__DIR__, "../../data/graphene_examples/wannierbands.txt")
-    map_dir = joinpath(@__DIR__, "../../data/graphene_examples/wanniercellmap.txt")
+    bands_dir = joinpath(@__DIR__, dir*"wannierbands.txt")
+    map_dir = joinpath(@__DIR__, dir*"wanniercellmap.txt")
     for i in 1:10
         println(i);bands[:, i] = wannier_bands(bands_dir, map_dir, [0, 0.5*i/10, 0], 8)
     end
@@ -27,13 +32,15 @@ function dft_graphene_wannier_dispersion()
     for i in 1:10
         println(i+20);bands[:, i+20] = wannier_bands(bands_dir, map_dir, [2/3-2/3*i/10, -1/3+1/3*i/10, 0], 8)
     end
-    return bands
+    return transpose(bands)
 end
 
+
 function dft_graphene_wannierbandsoverlayedDOS(mesh1::Integer=100, mesh2::Integer=1000)
-    bands_dir = joinpath(@__DIR__, "../../data/graphene_examples/wannierbands.txt")
-    map_dir = joinpath(@__DIR__, "../../data/graphene_examples/wanniercellmap.txt")
-    DOS_DATA_PATH = joinpath(@__DIR__, "../../data/graphene_examples/graphene.in.dos")
+    dir = "../../data/graphene_examples/"
+    bands_dir = joinpath(@__DIR__, dir*"wannierbands.txt")
+    map_dir = joinpath(@__DIR__, dir*"wanniercellmap.txt")
+    DOS_DATA_PATH = joinpath(@__DIR__, dir*"graphene.in.dos")
     Hwannier = hwannier(bands_dir, map_dir, 8)
     cellmap = np.loadtxt(map_dir)
     bands = zeros(8, 300)
@@ -46,7 +53,9 @@ function dft_graphene_wannierbandsoverlayedDOS(mesh1::Integer=100, mesh2::Intege
     for i in 1:100
         bands[:, i+200] = wannier_bands(Hwannier, cellmap, [2/3-2/3*i/100, -1/3+1/3*i/100, 0], 8)
     end
-    C = plot(2*density_of_states_wannier(Hwannier, cellmap, 8, histogram_width = 5, mesh = mesh1, offset = 25, energy_range=35), collect(0-25:1/5:35-25-1/5),  legend = false, title = "Wannier DOS", linewidth = 4)
+    dosW = density_of_states_wannier(Hwannier, cellmap, 8, histogram_width = 5, mesh = mesh1, offset = 25, energy_range=35)
+
+    C = plot(2*dosW[2], dosW[1], legend = false, title = "Wannier DOS", linewidth = 4)
     analyticgraphenedos = 2*graphene_dos(-2.8, mesh2, 5)
     D = plot(analyticgraphenedos,1:length(analyticgraphenedos), yticks = false, linewidth=4, title = "Dirac Cone Model DOS", legend = false)
     A = plot(transpose(bands), ylabel = "Energy (eV)", legend=false, linewidth = 4, xticks = false)
@@ -57,20 +66,21 @@ end
 
 function graphene_eph_matrix_elements(k1::Vector{<:Real}, k2::Vector{<:Real}, PhononBand::Integer)
 
-    bands_dir = joinpath(@__DIR__, "../../data/graphene_examples/wannierbands.txt")
-    map_dir = joinpath(@__DIR__, "../../data/graphene_examples/wanniercellmap.txt")
+    dir = "../../data/graphene_examples"
+    bands_dir = joinpath(@__DIR__, dir*"/wannierbands.txt")
+    map_dir = joinpath(@__DIR__, dir*"wanniercellmap.txt")
 
-    cell_map_dir = joinpath(@__DIR__, "../../data/graphene_examples/wannier.graphene.in.mlwfCellMap")
-    cell_weights_dir = joinpath(@__DIR__, "../../data/graphene_examples/wannier.graphene.in.mlwfCellWeights")
+    cell_map_dir = joinpath(@__DIR__, dir*"wannier.graphene.in.mlwfCellMap")
+    cell_weights_dir = joinpath(@__DIR__, dir*"wannier.graphene.in.mlwfCellWeights")
 
-    cell_mapph_dir = joinpath(@__DIR__, "../../data/graphene_examples/wannier.graphene.in.mlwfCellMapPh")
-    cell_weightsph_dir = joinpath(@__DIR__, "../../data/graphene_examples/wannier.graphene.in.mlwfCellWeightsPh")
-    HePh_dir = joinpath(@__DIR__, "../../data/graphene_examples/wannier.graphene.in.mlwfHePh")
+    cell_mapph_dir = joinpath(@__DIR__, dir*"wannier.graphene.in.mlwfCellMapPh")
+    cell_weightsph_dir = joinpath(@__DIR__, dir*"wannier.graphene.in.mlwfCellWeightsPh")
+    HePh_dir = joinpath(@__DIR__, dir*"wannier.graphene.in.mlwfHePh")
 
-    phonon_cellmap_dir = joinpath(@__DIR__, "../../data/graphene_examples/graphene.in.phononCellMap")
-    phonon_omegasq_dir = joinpath(@__DIR__, "../../data/graphene_examples/graphene.in.PhononOmegaSq")
+    phonon_cellmap_dir = joinpath(@__DIR__, dir*"graphene.in.phononCellMap")
+    phonon_omegasq_dir = joinpath(@__DIR__, dir*"graphene.in.PhononOmegaSq")
 
-    HePhWannier, cellMapEph=write_eph_matrix_elements(cell_map_dir, cell_weights_dir, cell_mapph_dir, cell_weightsph_dir, HePh_dir, 6, [2, 2, 1])
+    HePhWannier, cellMapEph = write_eph_matrix_elements(cell_map_dir, cell_weights_dir, cell_mapph_dir, cell_weightsph_dir, HePh_dir, 6, [2, 2, 1])
     forcemat, mapph = phonon_force_matrix(phonon_cellmap_dir, phonon_omegasq_dir)
 
     return abs.(eph_matrix_elements(HePhWannier, cellMapEph, forcemat, mapph, bands_dir, map_dir, k1, k2, 8)[PhononBand, 4, :])
@@ -81,15 +91,16 @@ function graphene_eph_matrix_elements(HePhWannier, cellMapEph, forcemat, mapph, 
 end
 
 function graphene_eph_matrix_elements_compare(numpoints::Integer)
-    bands_dir = joinpath(@__DIR__, "../../data/graphene_examples/wannierbands.txt")
-    map_dir = joinpath(@__DIR__, "../../data/graphene_examples/wanniercellmap.txt")
-    cell_map_dir = joinpath(@__DIR__, "../../data/graphene_examples/wannier.graphene.in.mlwfCellMap")
-    cell_weights_dir = joinpath(@__DIR__, "../../data/graphene_examples/wannier.graphene.in.mlwfCellWeights")
-    cell_mapph_dir = joinpath(@__DIR__, "../../data/graphene_examples/wannier.graphene.in.mlwfCellMapPh")
-    cell_weightsph_dir = joinpath(@__DIR__, "../../data/graphene_examples/wannier.graphene.in.mlwfCellWeightsPh")
-    HePh_dir = joinpath(@__DIR__, "../../data/graphene_examples/wannier.graphene.in.mlwfHePh")
-    phonon_cellmap_dir = joinpath(@__DIR__, "../../data/graphene_examples/graphene.in.phononCellMap")
-    phonon_omegasq_dir = joinpath(@__DIR__, "../../data/graphene_examples/graphene.in.PhononOmegaSq")
+    dir = "../../data/graphene_examples/"
+    bands_dir = joinpath(@__DIR__, dir*"wannierbands.txt")
+    map_dir = joinpath(@__DIR__, dir*"wanniercellmap.txt")
+    cell_map_dir = joinpath(@__DIR__, dir*"wannier.graphene.in.mlwfCellMap")
+    cell_weights_dir = joinpath(@__DIR__, dir*"wannier.graphene.in.mlwfCellWeights")
+    cell_mapph_dir = joinpath(@__DIR__, dir*"wannier.graphene.in.mlwfCellMapPh")
+    cell_weightsph_dir = joinpath(@__DIR__, dir*"wannier.graphene.in.mlwfCellWeightsPh")
+    HePh_dir = joinpath(@__DIR__, dir*"wannier.graphene.in.mlwfHePh")
+    phonon_cellmap_dir = joinpath(@__DIR__, dir*"graphene.in.phononCellMap")
+    phonon_omegasq_dir = joinpath(@__DIR__, dir*"graphene.in.PhononOmegaSq")
     HePhWannier, cellMapEph=write_eph_matrix_elements(cell_map_dir, cell_weights_dir, cell_mapph_dir, cell_weightsph_dir, HePh_dir, 6, [2, 2, 1])
     forcemat, mapph = phonon_force_matrix(phonon_cellmap_dir, phonon_omegasq_dir)
     K=[2/3, -1/3, 0]
@@ -132,29 +143,36 @@ function graphene_eph_matrix_elements_compare(numpoints::Integer)
 
 end
 
+"""
+A simple example to show the normalization of JDFTX data- This function should give a value close to 16 (8 bands and each has two possible spins)
+"""
 function graphene_dos_check()
-    DOS_DATA_PATH = joinpath(@__DIR__, "../../data/graphene_examples/graphene.in.dos")
+    dir = "../../data/graphene_examples/"
+    DOS_DATA_PATH = joinpath(@__DIR__, dir*"graphene.in.dos")
     x, y = np.loadtxt(DOS_DATA_PATH)[:, 1]*27.2, np.loadtxt(DOS_DATA_PATH)[:, 2]/27.2
     sum(y[2:end].*diff(x))
 end
 
 function graphene_wannier_impolarization(qx::Real; mesh::Integer = 20, histogram_width::Real = 10)
     a = 1.42*sqrt(3)
-    bands_dir = joinpath(@__DIR__, "../../data/graphene_examples/wannierbands.txt")
-    map_dir = joinpath(@__DIR__, "../../data/graphene_examples/wanniercellmap.txt")
+    dir = "../../data/graphene_examples/"
+    bands_dir = joinpath(@__DIR__, dir*"wannierbands.txt")
+    map_dir = joinpath(@__DIR__, dir*"wanniercellmap.txt")
     HWannier=hwannier(bands_dir, map_dir, 8);
     cell_map=np.loadtxt(map_dir);
     im_pols = im_polarization(HWannier, cell_map, 8, 4, [[a, 0, 0], [-a/2, a*sqrt(3)/2, 0], [0, 0, 10]], [qx, 0, 0], -3; 
         spin=4, Koffset=[2/3, -1/3, 0], subset=10, mesh=mesh, histogram_width=histogram_width) 
-    #im_pols = im_polarization_mc(HWannier, cell_map, 8, 4, [[a, 0, 0], [-a/2, a*sqrt(3)/2, 0], [0, 0, 10]], [qx, 0, 0], -3;  spin=2, mesh=mesh, histogram_width=histogram_width) 
     return im_pols
 end
 
+"""
+Given as an example of the general procedure to find the plasmon dispersion. First, the imaginary value of the polarization is found
+"""
 function example_graphene_wannier_plasmon(nqs::Integer, nomegas::Integer; mesh::Integer=30)
     plasmon = zeros(nqs, nomegas)
     for i in 1:nqs
         println(i)
-        gimpol=graphene_wannier_impolarization(i/nqs*1/6, mesh=mesh, histogram_width=100)
+        gimpol = graphene_wannier_impolarization(i/nqs*1/6, mesh=mesh, histogram_width=100)
         for j in 1:nomegas
             plasmon[i, j] = return_2d_epsilon_scipy(i/nqs*1/6, 2*j/nomegas, smooth(gimpol, win_len=10), 100, 100, 30)
         end
@@ -197,14 +215,14 @@ function example_al_wannier_bands()
 end    
 
 function example_aluminum_imepsilon(;histogram_width::Integer=10, mesh::Integer=10)
-    wannier_bands_path = joinpath(@__DIR__, "../../data/momentum_matrix_elements/Al_wannierbands.txt")
-    cell_map_path = joinpath(@__DIR__, "../../data/momentum_matrix_elements/Al_cellmap.txt")
-    Pwannier_path = joinpath(@__DIR__, "../../data/momentum_matrix_elements/AlP.txt")
+    dir = "../../data/momentum_matrix_elements/"
+    wannier_bands_path = joinpath(@__DIR__, dir*"Al_wannierbands.txt")
+    cell_map_path = joinpath(@__DIR__, dir*"Al_cellmap.txt")
+    Pwannier_path = joinpath(@__DIR__, dir*"AlP.txt")
     Al_cellmap = np.loadtxt(cell_map_path)
     Al_hwannier = hwannier(wannier_bands_path, cell_map_path, 5)
     AlPwannier = pwannier(Pwannier_path, cell_map_path, 5)
     a = 4.05
     lattice_vectors = [[a/2, a/2, 0], [0, a/2, a/2], [a/2, 0, a/2]]
-    #return im_epsilon_3d(lattice_vectors, Al_hwannier, Al_cellmap, AlPwannier, 5, 11; mesh=mesh, spin=2, histogram_width= histogram_width )
     return im_epsilon_3d_mc(lattice_vectors, Al_hwannier, Al_cellmap, AlPwannier, 5, 11; mesh=mesh, spin=2, histogram_width= histogram_width )
 end
