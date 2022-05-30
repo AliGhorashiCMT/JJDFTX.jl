8d68f9d1c2d5786356ac5c8eedb48a6298fead28
Ali Ghorashi <aligho@mit.edu>
2022-05-30 16:31:29 -0400

    All exporting methods now in pure Julia

--- src/export_wannier_hamiltonians.jl
+++ src/export_wannier_hamiltonians.jl
@@ -1,53 +1,15 @@
 """
 $(TYPEDSIGNATURES)
 """
-function write_map_write_h(filebase::AbstractString, kmesh::Vector{<:Real}; 
-    spin::Union{Val{'u'}, Val{'d'}, Val{'n'}}=Val('n'))
-    if spin isa Val{'u'}
-        cell_map = "$filebase.mlwfCellMapUp"
-        cell_weights = "$filebase.mlwfCellWeightsUp"
-        H = "$filebase.mlwfHUp"
-        band_file = "$(filebase)Up.txt"
-        cell_map_file = "$(filebase)Up.map.txt"
-    elseif spin isa Val{'d'}
-        cell_map = "$filebase.mlwfCellMapDn"
-        cell_weights = "$filebase.mlwfCellWeightsDn"
-        H = "$filebase.mlwfHDn"
-        band_file = "$(filebase)Dn.txt"
-        cell_map_file = "$(filebase)Dn.map.txt"
-    elseif spin isa Val{'n'}
-        cell_map = "$filebase.mlwfCellMap"
-        cell_weights = "$filebase.mlwfCellWeights"
-        H = "$filebase.mlwfH"
-        band_file = "$filebase.txt"
-        cell_map_file = "$filebase.map.txt"
-    end
-    py"""   
-    def write_map_write_h_py(cell_map, cell_weights, H, kmesh, band_file, cell_map_file):
-        import numpy as np
-        cellMapUp = np.loadtxt(cell_map)[:,0:3].astype(np.int)
-        WwannierUp = np.fromfile(cell_weights)
-        nCellsUp = cellMapUp.shape[0]
-        nBandsUp = int(np.sqrt(WwannierUp.shape[0] / nCellsUp))
-        WwannierUp = WwannierUp.reshape((nCellsUp,nBandsUp,nBandsUp)).swapaxes(1,2)
-        kfold=np.array([kmesh[0], kmesh[1], kmesh[2]])
-        kfoldProd = np.prod(kfold)
-        kStride = np.array([kfold[1]*kfold[2], kfold[2], 1])
-        HreducedUp = np.fromfile(H).reshape((kfoldProd,nBandsUp,nBandsUp)).swapaxes(1,2)
-        iReducedUp = np.dot(np.mod(cellMapUp, kfold[None,:]), kStride)
-        HwannierUp = WwannierUp * HreducedUp[iReducedUp]
-        np.savetxt(band_file, HwannierUp.reshape(len(iReducedUp), nBandsUp*nBandsUp ))
-        np.savetxt(cell_map_file, cellMapUp)
-    """
-    py"write_map_write_h_py"(cell_map, cell_weights, H, kmesh, band_file, cell_map_file)
-end
-
-"""
-$(TYPEDSIGNATURES)
-"""
 function export_hwannier(filebase::AbstractString, kmesh::Vector{<:Real}; 
     spin::Union{Val{'u'}, Val{'d'}, Val{'n'}}=Val('n'))
-    if spin isa Val{'u'}
+    if spin isa Val{'n'}
+        cell_map = "$filebase.mlwfCellMap"
+        cell_weights = "$filebase.mlwfCellWeights"
+        H = "$filebase.mlwfH"
+        band_file = "$filebase.txt"
+        cell_map_file = "$filebase.map.txt"
+    elseif spin isa Val{'u'}
         cell_map = "$filebase.mlwfCellMapUp"
         cell_weights = "$filebase.mlwfCellWeightsUp"
         H = "$filebase.mlwfHUp"
@@ -59,13 +21,8 @@ function export_hwannier(filebase::AbstractString, kmesh::Vector{<:Real};
         H = "$filebase.mlwfHDn"
         band_file = "$(filebase)Dn.txt"
         cell_map_file = "$(filebase)Dn.map.txt"
-    elseif spin isa Val{'n'}
-        cell_map = "$filebase.mlwfCellMap"
-        cell_weights = "$filebase.mlwfCellWeights"
-        H = "$filebase.mlwfH"
-        band_file = "$filebase.txt"
-        cell_map_file = "$filebase.map.txt"
     end
+
     cellMap = Int.(np.loadtxt(cell_map)[:,1:3])
     Wwannier = np.fromfile(cell_weights)
     nCells = first(size(cellMap))
@@ -81,95 +38,111 @@ function export_hwannier(filebase::AbstractString, kmesh::Vector{<:Real};
     np.savetxt(cell_map_file, cellMap)
 end
 
-
 """
 $(TYPEDSIGNATURES)
+Exports momentum file in format filebase.momentum.txt
 
 """
-function write_momentum(filebase::AbstractString, kmesh::Vector{<:Integer}, momentum_file::AbstractString; 
+function export_momentum(filebase::AbstractString, kmesh::Vector{<:Integer}; 
     spin::Union{Val{'u'}, Val{'d'}, Val{'n'}}=Val('n'))
     
-    if spin isa Val{'u'}
+    if spin isa Val{'n'}
         cell_map = filebase*".mlwfCellMap"
         cell_weights = filebase*".mlwfCellWeights"
         H = filebase*".mlwfH"
         P = filebase*".mlwfP"
+        momentum_file = filebase*".momentum.txt"
     elseif spin isa Val{'u'}
         cell_map = filebase*".mlwfCellMapUp"
         cell_weights = filebase*".mlwfCellWeightsUp"
         H = filebase*".mlwfHUp"
         P = filebase*".mlwfPUp"
+        momentum_file = filebase*"Up.momentum.txt"
     elseif spin isa Val{'d'}
         cell_map = filebase*".mlwfCellMapDn"
         cell_weights = filebase*".mlwfCellWeightsDn"
         H = filebase*".mlwfHDn"
         P = filebase*".mlwfPDn"    
+        momentum_file = filebase*"Dn.momentum.txt"
     end
-    py"""
-    def write_map_write_p(cell_map, cell_weights, H, P, kmesh, momentum_file):
-        import numpy as np
-        cellMap = np.loadtxt(cell_map)[:,0:3].astype(np.int)
-        Wwannier = np.fromfile(cell_weights)
-        nCells = cellMap.shape[0]
-        nBands = int(np.sqrt(Wwannier.shape[0] / nCells))
-        Wwannier = Wwannier.reshape((nCells,nBands,nBands)).swapaxes(1,2)
-        kfold=np.array([kmesh[0], kmesh[1], kmesh[2]])
-        kfoldProd = np.prod(kfold)
-        kStride = np.array([kfold[1]*kfold[2], kfold[2], 1])
-        Hreduced = np.fromfile(H).reshape((kfoldProd,nBands,nBands)).swapaxes(1,2)
-        Preduced = np.fromfile(P).reshape((kfoldProd,3,nBands,nBands)).swapaxes(2,3)
-        iReduced = np.dot(np.mod(cellMap, kfold[None,:]), kStride)
-        Hwannier = Wwannier * Hreduced[iReduced]
-        Pwannier = Wwannier[:,None] * Preduced[iReduced]
-        np.savetxt(momentum_file, Pwannier.reshape(len(iReduced), 3*nBands*nBands ))
-    """
-    py"write_map_write_p"(cell_map, cell_weights, H, P, kmesh, momentum_file)
+    cellMap = Int.(np.loadtxt(cell_map)[:,1:3])
+    Wwannier = np.fromfile(cell_weights)
+    nCells = first(size(cellMap))
+    nBands = Int(sqrt(length(Wwannier) / nCells))
+    Wwannier = reshape(permutedims(reshape(Wwannier, (nBands*nBands, nCells)), (2, 1)), (nCells, nBands, nBands))
+    kfoldProd = prod(kmesh)
+    kStride = [kmesh[2]*kmesh[3], kmesh[3], 1]
+    iReduced = np.mod(cellMap, kmesh)*kStride
+    Preduced = np.fromfile(P)
+    Preduced = permutedims(reshape(np.fromfile(P), (3*nBands*nBands, kfoldProd)), (2, 1))
+    Preduced = permutedims(reshape(Preduced, (kfoldProd, nBands, 3*nBands)), (1, 3, 2)) 
+    Preduced = permutedims(reshape(Preduced, (kfoldProd, nBands, 3, nBands)), (1, 3, 4, 2 ))
+    Pwannier = np.einsum("ijk, izjk -> izjk", Wwannier, Preduced[iReduced .+ 1, :,  :, :])
+    np.savetxt(momentum_file, np.reshape(Pwannier, (length(iReduced), 3*nBands*nBands)))
 end
 
 """
 $(TYPEDSIGNATURES)
+
+Exports heph, cellmapeph in files with pattern filebase.heph.txt and filebase.mapeph.txt
 """
-function write_eph_matrix_elements(filebase::AbstractString, nModes::Integer, qmesh::Vector{<:Integer}; 
+function export_heph(filebase::AbstractString, nModes::Integer, qmesh::Vector{<:Integer}; 
     spin::Union{Val{'u'}, Val{'d'}, Val{'n'}}=Val('n'))
     
-    if spin isa Val{'u'}
+    if spin isa Val{'n'}
         cell_map = "$filebase.mlwfCellMap"
         cell_weights = "$filebase.mlwfCellWeights"
         cell_map_ph = "$filebase.mlwfCellMapPh"
         cell_map_ph_weights = "$filebase.mlwfCellWeightsPh"
         HPh = "$filebase.mlwfHePh"
+        heph_file = "$filebase.heph.txt"
+        celleph_file = "$filebase.mapeph.txt"
     elseif spin isa Val{'u'}
         cell_map = "$filebase.mlwfCellMapUp"
         cell_weights = "$filebase.mlwfCellWeightsUp"
         cell_map_ph = "$filebase.mlwfCellMapPhUp"
         cell_map_ph_weights = "$filebase.mlwfCellWeightsPhUp"
         HPh = "$filebase.mlwfHePhUp"        
+        heph_file = "$(filebase)Up.heph.txt"
+        celleph_file = "$(filebase)Up.mapeph.txt"
     elseif spin isa Val{'d'}
         cell_map = "$filebase.mlwfCellMapDn"
         cell_weights = "$filebase.mlwfCellWeightsDn"
         cell_map_ph = "$filebase.mlwfCellMapPhDn"
         cell_map_ph_weights = "$filebase.mlwfCellWeightsPhDn"
-        HPh = "$filebase.mlwfHePhDn"    
+        HPh = "$filebase.mlwfHePhDn"   
+        heph_file = "$(filebase)Dn.heph.txt"
+        celleph_file = "$(filebase)Dn.mapeph.txt"
     end
-    py"""
-    def write_eph(cell_map, cell_weights, cell_map_ph, cell_map_ph_weights, HPh, nModes, qmesh):
-        import numpy as np
-        cellMap = np.loadtxt(cell_map)[:,0:3].astype(np.int)
-        Wwannier = np.fromfile(cell_weights)
-        nCells = cellMap.shape[0]
-        nBands = int(np.sqrt(Wwannier.shape[0] / nCells))
-        cellMapEph = np.loadtxt(cell_map_ph, usecols=[0,1,2]).astype(int)
-        nCellsEph = cellMapEph.shape[0]
-        prodPhononSup = np.prod(qmesh)
-        phononSupStride = np.array([qmesh[1]*qmesh[2], qmesh[2], 1])
-        nAtoms = nModes // 3 # Rounding down to an integer, since we know the number of modes is a multiple of three
-        cellWeightsEph = np.fromfile(cell_map_ph_weights).reshape((nCellsEph,nBands,nAtoms)).swapaxes(1,2)
-        cellWeightsEph = np.repeat(cellWeightsEph.reshape((nCellsEph,nAtoms,1,nBands)), 3, axis=2) #repeat atom weights for 3 directions
-        cellWeightsEph = cellWeightsEph.reshape((nCellsEph,nModes,nBands)) #coombine nAtoms x 3 into single dimension: nModes
-        iReducedEph = np.dot(np.mod(cellMapEph, qmesh[None,:]), phononSupStride)
-        HePhReduced = np.fromfile(HPh).reshape((prodPhononSup,prodPhononSup,nModes,nBands,nBands)).swapaxes(3,4)
-        HePhWannier = cellWeightsEph[:,None,:,:,None] * cellWeightsEph[None,:,:,None,:] * HePhReduced[iReducedEph][:,iReducedEph]
-        return HePhWannier, cellMapEph
-    """
-    py"write_eph"(cell_map, cell_weights, cell_map_ph, cell_map_ph_weights, HPh, nModes, qmesh)
+
+    cellMap = Int.(np.loadtxt(cell_map)[:,1:3])
+    Wwannier = np.fromfile(cell_weights)
+    nCells = first(size(cellMap))
+    nBands = Int(sqrt(length(Wwannier) / nCells))
+
+    cellMapEph = Int.(np.loadtxt(cell_map_ph, usecols=[0,1,2]))
+    nCellsEph = first(size(cellMapEph))
+    prodPhononSup = prod(qmesh)
+    phononSupStride = [qmesh[1]*qmesh[2], qmesh[2], 1]
+    nAtoms = div(nModes, 3) # Rounding down to an integer, since we know the number of modes is a multiple of three
+    cellWeightsEph = np.fromfile(cell_map_ph_weights)
+    cellWeightsEph = reshape(permutedims(reshape(cellWeightsEph, (nBands*nAtoms, nCellsEph)), (2, 1)), (nCellsEph, nAtoms, nBands))
+
+    cellWeightsEph = np.repeat(reshape(cellWeightsEph, (nCellsEph,nAtoms,1,nBands)), 3, axis=2) #repeat atom weights for 3 directions
+    
+    cellWeightsEph = reshape(permutedims(cellWeightsEph, (1, 3, 2, 4)), (nCellsEph, nModes,nBands)) #coombine nAtoms x 3 into single dimension: nModes
+        
+    iReducedEph = np.mod(cellMapEph, qmesh)*phononSupStride
+
+    HePhReduced = np.fromfile(HPh)
+
+    HePhReduced = permutedims(reshape(HePhReduced, (nModes*nBands^2, prodPhononSup^2)), (2, 1))
+    HePhReduced = permutedims(reshape(HePhReduced, (prodPhononSup, prodPhononSup, nModes*nBands^2)), (2, 1, 3))
+    HePhReduced = permutedims(reshape(HePhReduced, (prodPhononSup, prodPhononSup, nBands^2, nModes)), (1, 2, 4, 3))
+    HePhReduced = reshape(HePhReduced, (prodPhononSup, prodPhononSup, nModes, nBands, nBands))
+
+    HePhWannier = np.einsum("ikl, jkm -> ijklm", cellWeightsEph, cellWeightsEph)
+    HePhWannier = HePhWannier .* HePhReduced[iReducedEph  .+ 1, :, :, :, :][:, iReducedEph .+ 1, :, :, :]
+    np.savetxt(heph_file, np.reshape(HePhWannier, (length(iReducedEph)^2, -1)))
+    np.savetxt(celleph_file, cellMapEph)
 end
