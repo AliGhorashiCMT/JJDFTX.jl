3cb14a9c22ef11136a0791f6550b942e28c58156
Ali Ghorashi <aligho@mit.edu>
2022-05-31 21:14:53 -0400

    Faster diagonalization for phonons

--- src/phonon_properties.jl
+++ src/phonon_properties.jl
@@ -1,102 +1,54 @@
 """
 $(TYPEDSIGNATURES)
 
+"""
+function phonon_force_matrix(phonon_cell_map::AbstractString, phononOmegaSq::AbstractString)
+    cellph_map = np.loadtxt(phonon_cell_map)[:,1:3]
+    force_matrix = np.fromfile(phononOmegaSq, dtype=np.float64)
+    num_cells = first(size(cellph_map))
+    num_modes = Int(np.sqrt(first(size(force_matrix)) / num_cells))
+    force_matrix = np.reshape(force_matrix, (num_cells, num_modes, num_modes))
+    force_matrix = permutedims(force_matrix, (1, 3, 2))
+    return force_matrix, cellph_map
+end
+
+phonon_force_matrix(filebase::AbstractString) = phonon_force_matrix("$(filebase).phononCellMap", "$(filebase).phononOmegaSq")
+
+"""
+$(TYPEDSIGNATURES)
+"""
+function diagonalize_phonon(force_matrix::Array{<:Real, 3}, cellph_map::Array{<:Real, 2}, k::Vector{<:Real})
+    phase = np.exp(2im*π*cellph_map*k)
+    omegaSq, U = np.linalg.eigh(np.tensordot(phase, force_matrix, axes=1))
+    return omegaSq, U
+end
+
+function diagonalize_phonon(force_matrix::Array{<:Real, 3}, cellph_map::Array{<:Real, 2}, kpoints::AbstractArray{<:Real, 2})
+    phase = np.exp(2im*π*cellph_map*kpoints)
+    omegaSqs, Us = np.linalg.eigh(np.einsum("kij, kl -> lij", force_matrix, phase))
+    return omegaSqs, Us
+end
+
+"""
+$(TYPEDSIGNATURES)
+
 Optional keyword argument return_negative may be supplied to return imaginary frequencies as negative frequencies
 
 """
-function phonon_dispersion(force_matrix::Array{<:Real, 3}, phonon_cell_map::Array{<:Real, 2}, qnorm::Vector{<:Real}; return_negative::Bool=false)
-    forceMatrixTildeq = np.tensordot(np.exp(2im*π*np.dot(qnorm, transpose(phonon_cell_map))), force_matrix, axes=1)
-    omegaSq, _ = np.linalg.eigh(forceMatrixTildeq)
+function phonon_dispersion(force_matrix::Array{<:Real, 3}, cellph_map::Array{<:Real, 2}, k::Vector{<:Real}; return_negative::Bool=false)
+    omegaSq, _ = diagonalize_phonon(force_matrix, cellph_map, k)
     freq = return_negative ? sign.(omegaSq).*sqrt.(abs.(omegaSq))/eV : sqrt.(abs.(omegaSq))/eV
     return freq
 end
 
-"""
-$(TYPEDSIGNATURES)
-Returns phonon dispersion along a supplied q-path
-"""
-function phonon_dispersionpath(force_matrix::Array{<:Real, 3}, phonon_cell_map::Array{<:Real, 2}, qnorms::Vector{<:Vector{<:Real}}; return_negative=false)
-    nmodes = length(phonon_dispersion(force_matrix, phonon_cell_map, [0, 0, 0]))
-    nks = length(qnorms)
-    ϵalongpath = zeros(nks, nmodes )
-    for (index, qnorm) in enumerate(qnorms)
-        ϵalongpath[index, :] = phonon_dispersion(force_matrix, phonon_cell_map, qnorm, return_negative=return_negative)
-    end
-    return ϵalongpath
+function phonon_dispersion(force_matrix::Array{<:Real, 3}, cellph_map::Array{<:Real, 2}, kpoints::AbstractArray{<:Real, 2}; return_negative::Bool=false)
+    omegaSq, _ = diagonalize_phonon(force_matrix, cellph_map, kpoints)
+    freq = return_negative ? sign.(omegaSq).*sqrt.(abs.(omegaSq))/eV : sqrt.(abs.(omegaSq))/eV
+    return freq
 end
 
-"""
-$(TYPEDSIGNATURES)
-
-Plots the phonon dispersion along a (possibly interpolated) Brillouin zone path as read from a jdftx convention bandstruct.kpoints file.
-"""
-function phonon_dispersionpath(force_matrix::Array{<:Real, 3}, phonon_cell_map::Array{<:Real, 2}; kpointsfile::AbstractString="bandstruct.kpoints", 
-    interpolate::Integer=1, plotbands::Bool=false, return_negative=false, kwargs...)
-    
-    nmodes = length(phonon_dispersion(force_matrix, phonon_cell_map, [0, 0, 0]))
-    qnorms = bandstructkpoints2q(kpointsfile=kpointsfile, interpolate=interpolate)
-    nks = length(qnorms)
-    ϵalongpath = zeros(nks, nmodes )
-    for (index, qnorm) in enumerate(qnorms)
-        ϵalongpath[index, :] = phonon_dispersion(force_matrix, phonon_cell_map, qnorm, return_negative=return_negative)
-    end
-    plotbands && plot(ϵalongpath; kwargs...)
-    return ϵalongpath
-end
-
-"""
-$(TYPEDSIGNATURES)
-"""
-function phonon_dispersionpath(force_matrix::Array{<:Real, 3}, phonon_cell_map::Array{<:Real, 2}, qnorms::Tuple{Vector{<:Vector{<:Real}}, Integer})
-    diffs = diff(qnorms[1])/qnorms[2]
-    allqnorms = Vector{Vector{Float64}}()
-    println(qnorms[2])
-    for (diff, k) in zip(diffs, qnorms[1][1:end-1])
-        for interp in 0:qnorms[2]-1
-            push!(allqnorms, k+interp*diff)
-        end
-    end
-    return phonon_dispersionpath(force_matrix, phonon_cell_map, allqnorms)
-end
-
-"""
-$(TYPEDSIGNATURES)
-"""
-function phonon_dispersionmodes(force_matrix::Array{<:Real, 3}, phonon_cell_map::Array{<:Real, 2}, qnorm::Vector{<:Real})
-    phase = np.exp((2im*np.pi)*np.tensordot(qnorm, transpose(phonon_cell_map), axes=1))
-    ### Note that we must permute the indices of the force matrix by jdftx convention
-    ### As in http://jdftx.org/EphMatrixElements.html
-    omegaSq, U = np.linalg.eigh(np.tensordot(phase, permutedims(force_matrix, (1, 3, 2)), axes=1))
-    return sqrt.(abs.(omegaSq))/eV, U
-end
-
-"""
-$(TYPEDSIGNATURES)
-
-"""
-function phonon_force_matrix(phonon_cell_map::AbstractString, phononOmegaSq::AbstractString)
-    cellMapPh = np.loadtxt(phonon_cell_map)[:,1:3]
-    forceMatrixPh = np.fromfile(phononOmegaSq, dtype=np.float64)
-    nCellsPh = size(cellMapPh)[1]
-    nModesPh = Int(np.sqrt(size(forceMatrixPh)[1] / nCellsPh))
-    forceMatrixPh = np.reshape(forceMatrixPh, (nCellsPh,nModesPh,nModesPh))
-    return forceMatrixPh, cellMapPh
-end
-
-"""
-$(TYPEDSIGNATURES)
-"""
-function phonon_force_matrix(filebase::AbstractString)
-    phonon_cell_map = "$(filebase).phononCellMap"
-    phononOmegaSq = "$(filebase).phononOmegaSq"
-    cellMapPh = np.loadtxt(phonon_cell_map)[:,1:3]
-    forceMatrixPh = np.fromfile(phononOmegaSq, dtype=np.float64)
-    nCellsPh = size(cellMapPh)[1]
-    nModesPh = Int(np.sqrt(size(forceMatrixPh)[1] / nCellsPh))
-    forceMatrixPh = np.reshape(forceMatrixPh, (nCellsPh,nModesPh,nModesPh))
-    println("Number of phonon modes is: ", nModesPh, "\nIf this is incorrect, something went wrong somewhere at some point.")
-    return forceMatrixPh, cellMapPh
-end
+phonon_dispersion(force_matrix::Array{<:Real, 3}, cellph_map::Array{<:Real, 2}; kpointsfile::AbstractString="bandstruct.kpoints", kwargs...) =
+phonon_dispersion(force_matrix, cellph_map, hcat(bandstructkpoints2q(kpointsfile=kpointsfile)...); kwargs...)
 
 """
 Returns the electron self energy to lowest order in the electron-phonon interaction. The expression used is from the paper:
