3af372c2e59ef9382c24a5c3e49834f0f29ab4b0
Ali Ghorashi <aligho@mit.edu>
2022-06-28 22:39:29 -0400

    Added drude test with revised method

--- test/Boltzmann.jl
+++ test/Boltzmann.jl
@@ -1,11 +1,16 @@
 @testset "Checking Intraband Conductivity of Graphene at Low Doping" begin
    dir = "../data/boltzmann/"
-   HWannier, cellmap = hwannier(dir*"wannier.in.txt", dir*"wannier.in.map.txt", 5), np.loadtxt(dir*"wannier.in.map.txt")
-   PWannier = pwannier(dir*"p.txt", dir*"wannier.in.map.txt", 5)
-   a=1.42
-   graphene_lattice=[[a*sqrt(3), 0, 0], [-a*sqrt(3)/2, a*3/2, 0], [0, 0, 10]]
-   numerical = drude_conductivity(graphene_lattice, HWannier, cellmap, PWannier, 5,-3.2, mesh=200, histogram_width=3)
-   check_against = 4*1/(π) #In terms of the universal conductivity
+
+   Hwannier, cell_map = hwannier("$dir/wannier"),  np.loadtxt("$dir/wannier.map.txt");
+   Pwannier = pwannier("$dir/wannier");   
+
+   x, y = drude_conductivity(lattice_vectors, Hwannier, cell_map, Pwannier, mesh=32, num_blocks=100,
+   histogram_width=20, degeneracy=2)   
+   
+   numerical = y[argmin(abs.(x .+ 4.225963500 .- 0.5)), 1, 1] 
+
+   check_against = 2*1/(π) #In terms of the universal conductivity
+
    @test (100*(numerical-check_against)/numerical) < 10 #Check for less than 10 percent difference 
 
 end
