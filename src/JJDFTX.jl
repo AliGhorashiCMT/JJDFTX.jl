94dcac99650428a827fb2b4cde5db7608c277fa9
Ali Ghorashi <75496694+AliGhorashiCMT@users.noreply.github.com>
2022-11-24 23:48:55 -0500

    Update JJDFTX.jl

--- src/JJDFTX.jl
+++ src/JJDFTX.jl
@@ -135,9 +135,6 @@ export landau_damping, first_order_damping, second_order_damping
 include("./read_prepared_data/read_prepared_data.jl")
 export dft_graphene_dos_per_area, dft_graphene_phonon_dispersion, graphene_dos_check, graphene_wannier_impolarization
 
-include("non_wannier_methods.jl")
-export nonwannier3dimepsilon, nonwannierimpol
-
 include("density_plotting.jl")
 export plot_density, plot_diffdensity, plot_wfns, wavefunctionoverlap, plot_dtot, plot_scfpotential
 
